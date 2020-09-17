abe87a6bbdb1552627427462bd0376fce62b18f0
Matthew.Harris <noreply>
2020-09-17 00:06:44 -0400

    Update test for report_export()

--- tests/testthat/test-report_export.R
+++ tests/testthat/test-report_export.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("export work", {
+  tmp <- xlreports::report_export(tabs = list(mtcars, airquality),
+                                 export_dest = "tmp_folder")
+  out <- list(readxl::read_excel(tmp, sheet = 1),
+              readxl::read_excel(tmp, sheet = 2))
+  unlink("tmp_folder", recursive = TRUE)
+  expect_equal(out, list(tidyr::as_tibble(mtcars),
+                         tidyr::as_tibble(airquality)))
 })
